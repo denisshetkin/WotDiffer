869815bc6ce43cd6650b287f821b69e38a329c4e
den <denis.shetkin@gmail.com>
2016-04-05 17:57:13 +0300

    added CompatibleModules

---
+++ WargamingApiManager/Entities/EncyclopediaDetails/WorldOfTanks/CompatibleModules.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace WargamingApiManager.Entities.EncyclopediaDetails.WorldOfTanks
+{
+  public class CompatibleModules
+  {
+    /// <summary>
+    /// Cost of research in experience
+    /// </summary>
+    [JsonProperty("module_id")]
+    public long? ModuleId { get; set; }
+
+    /// <summary>
+    /// Cost of research in experience
+    /// </summary>
+    [JsonProperty("is_default")]
+    public bool? IsDefault { get; set; }
+  }
+}
