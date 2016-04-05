9be69518afbac4d2f51cc4fb4d161c2063180919
den <denis.shetkin@gmail.com>
2016-04-05 22:37:41 +0300

    Added web app

---
+++ WotDifferWebApplication/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WotDifferWebApplication.MvcApplication" Language="C#" %>
---
+++ WotDifferWebApplication/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WotDifferWebApplication.Startup))]
+namespace WotDifferWebApplication
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
